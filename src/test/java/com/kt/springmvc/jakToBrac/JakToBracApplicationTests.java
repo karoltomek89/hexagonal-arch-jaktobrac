794640e62093b55e6de5a03e7bec79a7e9970b89
karoltomek <karoltomek@o2.pl>
2021-01-07 10:58:36 +0100

    initial commit

---
+++ src/test/java/com/kt/springmvc/jakToBrac/JakToBracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.springmvc.jakToBrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakToBracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
