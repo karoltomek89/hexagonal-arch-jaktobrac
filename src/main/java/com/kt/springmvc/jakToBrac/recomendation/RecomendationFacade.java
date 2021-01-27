ae545f9ca192c6fbdd0264b4c18038214b0b64c6
karoltomek <karoltomek@o2.pl>
2021-01-27 08:07:37 +0100

    updated and removed methods in RecomendationFacade

--- src/main/java/com/kt/springmvc/jakToBrac/recomendation/RecomendationFacade.java
+++ src/main/java/com/kt/springmvc/jakToBrac/recomendation/RecomendationFacade.java
@@ -2,21 +2,17 @@ package com.kt.springmvc.jakToBrac.recomendation;
 
 import com.kt.springmvc.jakToBrac.interaction.dto.InteractionFood;
 import com.kt.springmvc.jakToBrac.interaction.dto.InteractionDragsSuplements;
+import com.kt.springmvc.jakToBrac.product.dto.Product;
 import com.kt.springmvc.jakToBrac.recomendation.dto.Recomendation;
 
 import java.util.List;
 
 public class RecomendationFacade {
 
-    public void importInteractionTwoProductsList(List<InteractionDragsSuplements> interactionDragsSuplementsList){
-
-    }
-
-    public void importInteractionOneProductList(List<InteractionFood> interactionFoodList){
-
-    }
-
-    public Recomendation getRecomendation(){
+    public Recomendation getRecomendation(
+            List<Product> productList,
+            List<InteractionFood> interactionFoodList,
+            List<InteractionDragsSuplements> interactionDragsSuplementsList){
         return null;
     }
 
