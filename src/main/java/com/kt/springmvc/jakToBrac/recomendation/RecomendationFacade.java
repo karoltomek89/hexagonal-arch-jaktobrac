c2797b1d9af454123ca8680b699015cf29b6771f
karoltomek <karoltomek@o2.pl>
2021-01-29 07:48:42 +0100

    added requireNonNull for RecomendationFacade

--- src/main/java/com/kt/springmvc/jakToBrac/recomendation/RecomendationFacade.java
+++ src/main/java/com/kt/springmvc/jakToBrac/recomendation/RecomendationFacade.java
@@ -7,14 +7,19 @@ import com.kt.springmvc.jakToBrac.recomendation.dto.Recomendation;
 
 import java.util.List;
 
+import static java.util.Objects.requireNonNull;
+
 public class RecomendationFacade {
 
     public Recomendation getRecomendation(
             List<Product> productList,
             List<InteractionFood> interactionFoodList,
-            List<InteractionDragsSuplements> interactionDragsSuplementsList){
+            List<InteractionDragsSuplements> interactionDragsSuplementsList) {
 
-        return RecomendationCreator.createRecomendation(productList,interactionDragsSuplementsList, interactionFoodList);
+        requireNonNull(productList);
+        requireNonNull(interactionFoodList);
+        requireNonNull(interactionDragsSuplementsList);
+
+        return RecomendationCreator.createRecomendation(productList, interactionDragsSuplementsList, interactionFoodList);
     }
-
 }
